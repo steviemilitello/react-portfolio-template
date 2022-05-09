8cf684f580dd6e45ad01ce92d41dcebc078281eb
STEVIEMILITELLO <steviemilitello@gmail.com>
2022-05-09 16:30:28 -0400

    adds configs for deployment

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-plugin-netlify`
+    `gatsby-plugin-netlify`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
